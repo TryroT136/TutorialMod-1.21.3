381f98963dbf7579ad051d820dfdcdb7efe796b0
TryroT136 <tieg.abercrombie@gmail.com>
2024-11-23 12:00:36 -0700

    added pink garnet

--- src/main/java/net/tryrot136/tutorialmod/TutorialMod.java
+++ src/main/java/net/tryrot136/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.tryrot136.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tryrot136.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
